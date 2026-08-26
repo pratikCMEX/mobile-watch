5b142038295930b7b80433da28bf94f36aea2ba2
PMCME <pm.cmexpertise@gmail.com>
2026-08-26 17:07:10 +0530

    Update config.ts

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
+const MONGO_USERNAME = process.env.DB_USER || "";
+const MONGO_PASSWORD = process.env.DB_PASSWORD || "";
+
+const MONGO_URL = `mongodb://localhost/digitaInterpreter`;
 
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const TCP_PORT = process.env.TCP_PORT ? Number(process.env.TCP_PORT) : 9000;
 
 export const config = {
+    mongo: {
+        url: MONGO_URL,
+    },
     server: {
         port: SERVER_PORT,
     },
