578a75489bf02754f94106b138c44c823d514c1c
PMCME <pm.cmexpertise@gmail.com>
2026-08-26 16:57:07 +0530

    Update config.ts

--- app/config/config.ts
+++ app/config/config.ts
@@ -5,20 +5,18 @@ const MONGO_PASSWORD = process.env.DB_PASSWORD || "";
 
 const MONGO_URL = `mongodb://localshot/digitaInterpreter`;
 
-const SERVER_PORT = process.env.SERVER_PORT
-  ? Number(process.env.SERVER_PORT)
-  : 3006;
+const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3006;
 
 const TCP_PORT = process.env.TCP_PORT ? Number(process.env.TCP_PORT) : 9090;
 
 export const config = {
-  mongo: {
-    url: MONGO_URL,
-  },
-  server: {
-    port: SERVER_PORT,
-  },
-  tcp: {
-    port: TCP_PORT,
-  },
+    mongo: {
+        url: MONGO_URL,
+    },
+    server: {
+        port: SERVER_PORT,
+    },
+    tcp: {
+        port: TCP_PORT,
+    },
 };
