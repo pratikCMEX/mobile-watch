719008b19d432c8c62fde73a89e16d5c3d169d63
PMCME <pm.cmexpertise@gmail.com>
2026-08-26 17:10:31 +0530

    Update config.ts

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+//server config
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const TCP_PORT = process.env.TCP_PORT ? Number(process.env.TCP_PORT) : 9000;
