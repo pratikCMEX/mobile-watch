e75149813e01bb38c3fb31968b5c1d174493ac06
Vimal <vimal.cmexpertise@gmail.com>
2026-09-15 21:46:01 +0530

    Update Device_controller.ts

--- app/controllers/user/Device_controller.ts
+++ app/controllers/user/Device_controller.ts
@@ -2609,7 +2609,7 @@ const requestBodyTemperature = async function (
     if (serial_number) {
       device = await db.Device.findOne({
         where: { serial_number },
-        order: [["id", "DESC"]],
+        order: [["createdAt", "DESC"]],
       });
     }
 
