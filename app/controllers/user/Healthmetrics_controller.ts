102afe5f70d8a15b880af5a588e175f1554aea68
Vimal <vimal.cmexpertise@gmail.com>
2026-09-15 21:44:54 +0530

    Update Healthmetrics_controller.ts

--- app/controllers/user/Healthmetrics_controller.ts
+++ app/controllers/user/Healthmetrics_controller.ts
@@ -187,7 +187,7 @@ const getAnalytics = async (
         createdAt: { [Op.between]: [start, end] },
       },
       attributes: ["value_primary", "value_secondary", "unit", "recorded_at"],
-      order: [["value_primary", "ASC"]],
+      order: [["id", "DESC"]],
     });
 
     let summary = {
