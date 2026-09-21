abc5462f2d56e63c500cc3c8f65ceb8f628102e3
Vimal <vimal.cmexpertise@gmail.com>
2026-09-21 20:50:13 +0530

    Update notification.service.ts

--- app/services/notification.service.ts
+++ app/services/notification.service.ts
@@ -161,7 +161,7 @@ export const pushToUser = async (
   if (user.device_type === "ios") {
     response = await messaging().sendEachForMulticast({
       tokens,
-      notification: { body: "" },
+      notification: { title: "", body: "" },
       apns: {
         /* unchanged */
       },
